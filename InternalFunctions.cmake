ac47b24d153bd2eb6e44d61ec8e3720fe716598c
Michał Góral <virgoerns@gmail.com>
2012-08-05 20:21:48 +0200

    Fixed problems with test dependencies
    create_test function should handle creating test targets. Just assure
    that CMakeLists will pass ${TEST_DEPS} variables up to the one in src
    directory. Also, create_test should be invoked from appropriate
    CMakeLists, which will already include all needed dependencies.

---
+++ InternalFunctions.cmake
@@ -0,0 +1,32 @@
+function(create_test test_name link_libs)
+    set(TEST_BIN ${test_name})
+    set(TEST_TARGET ${test_name}_exe)
+
+    include_directories(
+        ${PROJECT_SOURCE_DIR}/thirdparty/gmock/include
+        ${PROJECT_SOURCE_DIR}/thirdparty/gmock/gtest/include
+    )
+
+    file(GLOB_RECURSE TEST_SOURCES tests/*.cpp)
+
+    add_executable(${TEST_BIN} EXCLUDE_FROM_ALL ${TEST_SOURCES})
+    target_link_libraries(
+        ${TEST_BIN}
+        gtest_main
+        gmock
+    )
+
+    foreach(lib IN LISTS ${link_libs})
+        target_link_libraries(${TEST_BIN} ${lib})
+    endforeach(lib)
+
+    add_custom_target(
+        ${TEST_TARGET}
+        "${CMAKE_CURRENT_BINARY_DIR}/${TEST_BIN}"
+        DEPENDS ${TEST_BIN}
+        VERBATIM
+    )
+
+    set(TEST_DEPS ${TEST_DEPS} ${TEST_TARGET} PARENT_SCOPE)
+endfunction(create_test)
+
