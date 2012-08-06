d9564218da87d0a2a46dad378c74793bf9688d35
Michał Góral <virgoerns@gmail.com>
2012-08-06 22:38:49 +0200

    Changed the way that JobUrl is constructed
    Job crashed during tests when copy ctor was used. Instead, wxURL is
    constructed as a completely new object from a given string.

--- src/core/include/Job.hpp
+++ src/core/include/Job.hpp
@@ -21,7 +21,7 @@ protected:
     virtual void updateHistory();
 
 public:
-    Job(JobName name, JobUrl url, JobStatus status = JOB_UNKNOWN, JobTime datetime = JobTime::Now());
+    Job(JobName name, wxString url, JobStatus status = JOB_UNKNOWN, JobTime datetime = JobTime::Now());
     virtual ~Job();
 
     HistoryElement getHistoryElement(u32 pos) const;
--- src/core/src/Job.cpp
+++ src/core/src/Job.cpp
@@ -7,8 +7,8 @@ namespace ci {
 namespace core {
 
 Job::Job(
-    JobName name, JobUrl url, JobStatus status, JobTime datetime
-) : name(name), url(url), status(datetime, status), maxHistorySize(defaultHistorySize)
+    JobName name, wxString urlStr, JobStatus status, JobTime datetime
+) : name(name), url(urlStr), status(datetime, status), maxHistorySize(defaultHistorySize)
 {}
 
 Job::~Job()
