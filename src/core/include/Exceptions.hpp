2b2368788e02d8977a499f2a61570acb62b680c3
Michał Góral <virgoerns@gmail.com>
2012-08-11 19:04:33 +0200

    Added namespace and missing semicolon

--- src/core/include/Exceptions.hpp
+++ src/core/include/Exceptions.hpp
@@ -5,13 +5,17 @@
 #include <wx/string.h>
 #include <string>
 
+namespace ci {
+
 class BadParameter : public std::invalid_argument {
 public:
     explicit BadParameter()
         : std::runtime_error("Incorrect parameter given.");
     explicit BadParameter(const wxString& message)
         : std::runtime_error(std::string(message.mb_str())) {};
-}
+};
+
+} // namespace ci
 
 #endif
 
