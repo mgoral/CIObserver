1999300e4e243e031d1131760be1e16fd2c9cfc7
Michał Góral <virgoerns@gmail.com>
2012-08-11 23:49:05 +0200

    Fixed compilation errors

--- src/core/include/Exceptions.hpp
+++ src/core/include/Exceptions.hpp
@@ -9,10 +9,10 @@ namespace ci {
 
 class bad_parameter : public std::invalid_argument {
 public:
-    explicit BadParameter()
-        : std::runtime_error("Incorrect parameter given.");
-    explicit BadParameter(const wxString& message)
-        : std::runtime_error(std::string(message.mb_str())) {};
+    explicit bad_parameter()
+        : std::invalid_argument("invalid parameter") {}
+    explicit bad_parameter(const wxString& message)
+        : std::invalid_argument(std::string(message.mb_str())) {}
 };
 
 } // namespace ci
