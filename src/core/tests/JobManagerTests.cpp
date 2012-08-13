7725e162ec73c3dbea0f79cb269ce5cfb727048c
Michał Góral <virgoerns@gmail.com>
2012-08-13 19:24:37 +0200

    Added JobManagerTests

--- src/core/tests/JobManagerTests.cpp
+++ src/core/tests/JobManagerTests.cpp
@@ -1,9 +1,11 @@
+#include <cstddef>
 #include <memory>
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 #include "Exceptions.hpp"
+#include "JobFactory.hpp"
 #include "JobManager.hpp"
 
 #include "IJobFactoryMock.hpp"
@@ -21,7 +23,7 @@ protected:
         name = "FooManagerName";
     }
 
-    JobManager::IJobFactoryPtr testJobFactory;
+    std::shared_ptr<IJobFactoryMock> testJobFactory;
     Url url;
     Name name;
 };
@@ -51,4 +53,66 @@ TEST_F(JobManagerTests, CheckDefaultManagerSettings) {
     EXPECT_EQ(url, manager.getName());
     EXPECT_EQ(url, manager.getUrl());
     EXPECT_EQ(Description(), manager.getDescription());
+}
+
+TEST_F(JobManagerTests, SetIncorrectUrl) {
+    JobManager manager(testJobFactory, url);
+    Url wrongUrl = "httpss:/WrongUrl.com";
+
+    ASSERT_FALSE(manager.setUrl(wrongUrl));
+}
+
+TEST_F(JobManagerTests, SetCorrectUrl) {
+    JobManager manager(testJobFactory, url);
+    Url newUrl = "https://example.org";
+
+    ASSERT_TRUE(manager.setUrl(newUrl));
+    ASSERT_EQ(newUrl, manager.getUrl());
+}
+
+TEST_F(JobManagerTests, SetDescription) {
+    JobManager manager(testJobFactory, url);
+    Description desc = "Job manager description containing strage Polish characters: ąuęśćź";
+
+    manager.setDescription(desc);
+    ASSERT_EQ(desc, manager.getDescription());
+}
+
+TEST_F(JobManagerTests, ChangeName) {
+    JobManager manager(testJobFactory, url);
+    Name name = "JobManager name";
+
+    manager.setName(name);
+    ASSERT_EQ(name, manager.getName());
+}
+
+TEST_F(JobManagerTests, AddJob) {
+    // We cannot use mocks here because it would mean that through created constant objects we're indirectly controlling
+    // tested methods behaviour
+    JobManager::IJobFactoryPtr jobFactory(new JobFactory());
+    JobManager manager(jobFactory, url);
+
+    Url jobUrl = "http://example.com";
+    Name jobName = "FooJob";
+    JobStatus jobStatus = JOB_OK;
+
+    manager.addJob(jobUrl, jobName, jobStatus);
+
+    ASSERT_EQ(jobUrl, manager.getJob(jobUrl)->getUrl());
+}
+
+TEST_F(JobManagerTests, RemoveJob) {
+    // We cannot use mocks here because it would mean that through created constant objects we're indirectly controlling
+    // tested methods behaviour
+    JobManager::IJobFactoryPtr jobFactory(new JobFactory());
+    JobManager manager(jobFactory, url);
+
+    Url jobUrl = "http://example.com";
+    Name jobName = "FooJob";
+    JobStatus jobStatus = JOB_OK;
+
+    manager.addJob(jobUrl, jobName, jobStatus);
+    manager.removeJob(jobUrl);
+
+    ASSERT_EQ(IJobPtr(), manager.getJob(jobUrl));
 }
