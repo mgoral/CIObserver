37abf8756d5f02e1f0c16762469083ff120b6c32
Michał Góral <virgoerns@gmail.com>
2012-08-15 19:57:33 +0200

    Removed ITimeObserver and ITimer

--- src/core/include/ITimeObserver.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef ITIMEOBSERVER_HPP
-#define ITIMEOBSERVER_HPP
-
-class ITimeObserver {
-
-};
-
-#endif
-
--- src/core/include/ITimer.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef ITIMER_HPP
-#define ITIMER_HPP
-
-class ITimer {
-
-};
-
-#endif
-
